0ea1331c60df17c122e575e5a81ee8a388218679
Louie Lee <louielee.dev@gmail.com>
2024-07-22 22:20:22 +0800

    feat: add parameters crud

---
+++ app/data/crud/parameters.ts
@@ -0,0 +1,76 @@
+import { createId } from "@paralleldrive/cuid2"
+import {
+  DBParameters,
+  PARAMETERS_TABLE_NAME,
+  Parameters,
+  ParametersCreate,
+  ParametersDelete,
+  ParametersRead,
+  ParametersUpdate,
+} from "../entities/parameters"
+import { DBAPI, DBStatement } from "./base"
+import { parseParamsToSqlParams } from "app/utils/sqlParameterParser"
+
+class ParametersStatement extends DBStatement {
+  constructor(tableName: string) {
+    super(tableName)
+  }
+  getLastParametersStatement() {
+    return `
+      SELECT * FROM ${PARAMETERS_TABLE_NAME} ORDER BY created_at DESC LIMIT 1
+    `
+  }
+}
+
+const parameters = new ParametersStatement(PARAMETERS_TABLE_NAME)
+
+export async function createParameters(
+  db: DBAPI,
+  { id = createId(), ...params }: ParametersCreate,
+): Promise<DBParameters> {
+  await db.run(parameters.getInsertStatement(params), {
+    $id: id,
+    ...parseParamsToSqlParams(params),
+  })
+
+  return db.get(parameters.getSelectStatement(), {
+    $id: id,
+  }) as Promise<DBParameters>
+}
+
+export async function findParameters(db: DBAPI): Promise<DBParameters[]> {
+  return db.find(parameters.getSelectAllStatement())
+}
+
+export async function getParameters(db: DBAPI, { id }: ParametersRead): Promise<Parameters> {
+  if (id) {
+    return db.get(parameters.getSelectStatement(), {
+      $id: id,
+    }) as Promise<Parameters>
+  } else {
+    return db.get(parameters.getLastParametersStatement()) as Promise<Parameters>
+  }
+}
+
+export async function updateParameters(
+  db: DBAPI,
+  { id, ...params }: ParametersUpdate,
+): Promise<DBParameters> {
+  const record = (await db.get(parameters.getSelectStatement(), {
+    $id: id,
+  })) as DBParameters
+
+  await db.run(parameters.getUpdateStatement(record), {
+    $id: id,
+    ...parseParamsToSqlParams(record),
+    ...parseParamsToSqlParams(params),
+  })
+
+  return db.get(parameters.getSelectStatement(), {
+    $id: id,
+  }) as Promise<DBParameters>
+}
+
+export async function deleteParameters(db: DBAPI, { id }: ParametersDelete) {
+  return db.run(parameters.getDeleteStatement(), { $id: id })
+}
---
+++ app/data/entities/parameters.ts
@@ -0,0 +1,46 @@
+import { createId } from "@paralleldrive/cuid2"
+import { DBTimestampBase, TimestampBase } from "./base"
+import { FSRSParameters, generatorParameters } from "ts-fsrs"
+
+export const PARAMETERS_TABLE_NAME = "parameters"
+
+export type DBParameters = DBTimestampBase & {
+  request_retention: number
+  maximum_interval: number
+  w: number[]
+  enable_fuzz: number
+  enable_short_term: number
+}
+
+export type Parameters = TimestampBase & {
+  request_retention: number
+  maximum_interval: number
+  w: number[]
+  enable_fuzz: boolean
+  enable_short_term: boolean
+}
+
+export type ParametersCreate = Partial<Parameters>
+export type ParametersRead = Partial<Parameters>
+export type ParametersUpdate = Partial<Parameters> & Pick<Parameters, "id">
+export type ParametersDelete = Pick<Parameters, "id">
+
+export const PARAMETERS_DDL = `
+  CREATE TABLE "${PARAMETERS_TABLE_NAME}" (
+    "id" TEXT PRIMARY KEY NOT NULL,
+    "request_retention" REAL DEFAULT 0.9 NOT NULL,
+    "maximum_interval" INTEGER DEFAULT 36500 NOT NULL,
+    "w" BLOB NOT NULL,
+    "enable_fuzz" INTEGER DEFAULT 0 NOT NULL,
+    "enable_short_term" INTEGER DEFAULT 1 NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT (DATETIME('NOW')),
+    "updated_at" DATETIME NOT NULL DEFAULT (DATETIME('NOW')),
+    "deleted_at" DATETIME
+)`
+
+const params: FSRSParameters = generatorParameters()
+
+export const PARAMETERS_SEED_STATEMENT = `
+  INSERT INTO ${PARAMETERS_TABLE_NAME} (id, w) 
+  VALUES ('${createId()}', '[${params.w}]')
+`
