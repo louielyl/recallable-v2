666301cbbf970bc0545979b16b65cde2b32d8fb4
Louie Lee <louielee.dev@gmail.com>
2024-07-22 22:20:30 +0800

    feat: add review logs crud

---
+++ app/data/crud/reviewLogs.ts
@@ -0,0 +1,81 @@
+import { createId } from "@paralleldrive/cuid2"
+import {
+  DBReviewLog,
+  REVIEW_LOGS_TABLE_NAME,
+  ReviewLogCreate,
+  ReviewLogDelete,
+  ReviewLogRead,
+  ReviewLogUpdate,
+} from "../entities/reviewLogs"
+import { DBAPI, DBStatement } from "./base"
+import { parseParamsToSqlParams } from "app/utils/sqlParameterParser"
+
+class ReviewLogStatement extends DBStatement {
+  constructor(tableName: string) {
+    super(tableName)
+  }
+
+  getSelectByHeadWordIdStatement() {
+    return `
+      SELECT * FROM ${this.tableName} WHERE head_word_id = $head_word_id
+    `
+  }
+}
+
+const revlewLog = new ReviewLogStatement(REVIEW_LOGS_TABLE_NAME)
+
+export async function createReviewLog(
+  db: DBAPI,
+  { id = createId(), ...params }: ReviewLogCreate,
+): Promise<DBReviewLog> {
+  await db.run(revlewLog.getInsertStatement(params), {
+    $id: id,
+    ...parseParamsToSqlParams(params),
+  })
+
+  return db.get(revlewLog.getSelectStatement(), {
+    $id: id,
+  }) as Promise<DBReviewLog>
+}
+
+export async function findReviewLogs(db: DBAPI): Promise<DBReviewLog[]> {
+  return db.find(revlewLog.getSelectAllStatement())
+}
+
+export async function getReviewLog(
+  db: DBAPI,
+  { head_word_id }: ReviewLogRead,
+): Promise<DBReviewLog> {
+  if (head_word_id)
+    return db.get(revlewLog.getSelectByHeadWordIdStatement(), {
+      $head_word_id: head_word_id,
+    }) as Promise<DBReviewLog>
+
+  throw new Error("Head word not found")
+}
+
+export async function updateReviewLog(
+  db: DBAPI,
+  { id, ...params }: ReviewLogUpdate,
+): Promise<DBReviewLog> {
+  const record = (await db.get(revlewLog.getSelectStatement(), {
+    $id: id,
+  })) as DBReviewLog
+
+  await db.run(revlewLog.getUpdateStatement(record), {
+    $id: id,
+    ...parseParamsToSqlParams(record),
+    ...parseParamsToSqlParams(params),
+  })
+
+  return db.get(revlewLog.getSelectStatement(), {
+    $id: id,
+  }) as Promise<DBReviewLog>
+}
+
+export async function deleteReviewLog(db: DBAPI, { id }: ReviewLogDelete) {
+  if (id)
+    return db.run(revlewLog.getDeleteStatement(), {
+      $id: id,
+    })
+}
---
+++ app/data/entities/reviewLogs.ts
@@ -0,0 +1,55 @@
+import { Rating, State } from "ts-fsrs"
+import { DBTimestampBase, TimestampBase } from "./base"
+import { HEAD_WORDS_TABLE_NAME } from "./headWords"
+
+export const REVIEW_LOGS_TABLE_NAME = "review_logs"
+
+export type DBReviewLog = DBTimestampBase & {
+  head_word_id: string
+  rating: number // Rating of the review (Again, Hard, Good, Easy)
+  state: number // State of the review (New, Learning, Review, Relearning)
+  due: string // Date of the last scheduling
+  stability: number // Stability of the card before the review
+  difficulty: number // Difficulty of the card before the review
+  elapsed_days: number // Number of days elapsed since the last review
+  last_elapsed_days: number // Number of days between the last two reviews
+  scheduled_days: number // Number of days until the next review
+  review: string | null // Date of the review
+}
+
+export type ReviewLog = TimestampBase & {
+  head_word_id: string
+  rating: Rating // Rating of the review (Again, Hard, Good, Easy)
+  state: State // State of the review (New, Learning, Review, Relearning)
+  due: Date // Date of the last scheduling
+  stability: number // Stability of the card before the review
+  difficulty: number // Difficulty of the card before the review
+  elapsed_days: number // Number of days elapsed since the last review
+  last_elapsed_days: number // Number of days between the last two reviews
+  scheduled_days: number // Number of days until the next review
+  review: Date // Date of the review
+}
+
+export type ReviewLogCreate = Partial<ReviewLog> & Pick<ReviewLog, "head_word_id">
+export type ReviewLogRead = Pick<ReviewLog, "head_word_id">
+export type ReviewLogUpdate = Pick<ReviewLog, "id">
+export type ReviewLogDelete = Pick<ReviewLog, "id">
+
+export const REVIEW_LOGS_DDL = `
+  CREATE TABLE "${REVIEW_LOGS_TABLE_NAME}" (
+    "id" TEXT PRIMARY KEY NOT NULL,
+    "head_word_id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "state" INTEGER NOT NULL,
+    "due" DATETIME NOT NULL,
+    "stability" REAL NOT NULL,
+    "difficulty" REAL NOT NULL,
+    "elapsed_days" INTEGER NOT NULL,
+    "last_elapsed_days" INTEGER NOT NULL,
+    "scheduled_days" INTEGER NOT NULL,
+    "review" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT (DATETIME('NOW')),
+    "updated_at" DATETIME NOT NULL DEFAULT (DATETIME('NOW')),
+    "deleted_at" DATETIME,
+    FOREIGN KEY(head_word_id) REFERENCES ${HEAD_WORDS_TABLE_NAME}(id)
+)`
