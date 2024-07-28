af65cf7e09f5eb2e9deb7d9d83fa98084bcf4ad7
Louie Lee <louielee.dev@gmail.com>
2024-07-28 18:54:16 +0800

    feat: update parser to store datestring

--- app/utils/sqlParameterParser.ts
+++ app/utils/sqlParameterParser.ts
@@ -1,6 +1,6 @@
 export function parseParamsToSqlParams(parameters: Record<string, any>): Record<string, any> {
   return Object.entries(parameters).reduce((acc: Record<string, any>, [key, value]) => {
-    acc["$" + key] = value
+    acc["$" + key] = value instanceof Date ? value.toISOString() : value
     return acc
   }, {})
 }
