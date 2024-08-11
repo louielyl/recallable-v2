57a8247db30ce0713684c2ffce994ed45e798d72
Louie Lee <louielee.dev@gmail.com>
2024-08-11 19:08:52 +0800

    feat: add buttons to front

--- app/screens/ReviewScreen/Front.tsx
+++ app/screens/ReviewScreen/Front.tsx
@@ -1,18 +1,33 @@
 import { FC } from "react"
-import { Screen, Text } from "app/components"
-import { ViewStyle } from "react-native"
+import { Button, Screen, Text } from "app/components"
+import { View, ViewStyle } from "react-native"
+import { Rating } from "ts-fsrs"
 
 type FrontProps = {
   content: string
+  buttonOnPress: (input: Rating) => void
 }
-export const Front: FC<FrontProps> = function ReviewScreen({ content }) {
+export const Front: FC<FrontProps> = function ReviewScreen({ content, buttonOnPress }) {
   return (
     <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContainer}>
       <Text text={content} />
+      <View style={$buttonContainer}>
+        <Button onPress={() => buttonOnPress(Rating.Good)} style={$button} text={"good"} />
+        <Button onPress={() => buttonOnPress(Rating.Hard)} style={$button} text={"hard"} />
+        <Button onPress={() => buttonOnPress(Rating.Again)} style={$button} text={"again"} />
+      </View>
     </Screen>
   )
 }
 
 const $screenContainer: ViewStyle = {
+  flex: 1,
+}
+
+const $buttonContainer: ViewStyle = {
+  flexDirection: "row",
+}
+
+const $button: ViewStyle = {
   flex: 1,
 }
