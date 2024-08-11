fc9356cfc2b095d7cdb8cf09b44fbc0a9a3ab22e
Louie Lee <louielee.dev@gmail.com>
2024-08-11 09:57:47 +0800

    feat: update cards by adding findSchedledCards

--- app/data/crud/cards.ts
+++ app/data/crud/cards.ts
@@ -10,15 +10,27 @@ import {
   CardSchedule,
   CardUpdate,
   DBCard,
+  ScheduledCard,
 } from "../entities/cards"
-import { FSRS, RecordLogItem, createEmptyCard, RecordLog, Grade } from "ts-fsrs"
+import { FSRS, RecordLogItem, createEmptyCard } from "ts-fsrs"
 import { getHeadWord } from "./headWords"
 import { getParameters } from "./parameters"
 import { createReviewLog } from "./reviewLogs"
+import { HEAD_WORDS_TABLE_NAME, HeadWord } from "../entities/headWords"
+import { HEAD_WORD_DEFINITION_MAPPINGS_TABLE_NAME } from "../entities/headWordDefinitionMappings"
+import { DEFINITIONS_TABLE_NAME, Definition } from "../entities/definitions"
+import { definitionStatementGeneartor } from "./definitions"
 
 class CardStatement extends DBStatement {
   constructor(tableName: string) {
     super(tableName)
+  }
+  findTodaysCards() {
+    return `
+      SELECT * from ${this.tableName}
+      WHERE date('now') >= date(due)
+      ORDER BY due DESC
+    `
   }
   getSelectByHeadWordId() {
     return `
@@ -30,7 +42,7 @@ class CardStatement extends DBStatement {
   }
 }
 
-const card = new CardStatement(CARDS_TABLE_NAME)
+const cardStatementGenerator = new CardStatement(CARDS_TABLE_NAME)
 
 function dbCardToCard(input: DBCard): Card {
   return Object.entries(input).reduce((acc, [key, value]) => {
@@ -55,22 +67,35 @@ export async function createCard(
 ): Promise<Card> {
   const emptyCard = createEmptyCard()
 
-  await db.run(card.getInsertStatement({ ...emptyCard, ...params }), {
+  await db.run(cardStatementGenerator.getInsertStatement({ ...emptyCard, ...params }), {
     $id: id,
     ...parseParamsToSqlParams(emptyCard),
     ...parseParamsToSqlParams(params),
   })
 
-  const dbCard = (await db.get(card.getSelectStatement(), {
+  const dbCard = (await db.get(cardStatementGenerator.getSelectStatement(), {
     $id: id,
   })) as DBCard
   return dbCardToCard(dbCard)
 }
 
+export async function findScheduledCards(db: DBAPI, { }): Promise<ScheduledCard[]> {
+  const cards: Card[] = await db.find(cardStatementGenerator.findTodaysCards())
+  return Promise.all(
+    cards.map(async (card) => {
+      const definitions: Definition[] = await db.find(
+        definitionStatementGeneartor.findDefinitionsByHeadWordId(card.head_word_id),
+      )
+      const headWord: HeadWord = await getHeadWord(db, { id: card.head_word_id })
+      return { ...card, definitions, headWord }
+    }),
+  )
+}
+
 export async function getCardByHeadWord(db: DBAPI, { content }: CardRead): Promise<Card> {
   const head_word_id = (await getHeadWord(db, { content })).id
 
-  const dbCard = await db.get(card.getSelectByHeadWordId(), {
+  const dbCard = await db.get(cardStatementGenerator.getSelectByHeadWordId(), {
     $head_word_id: head_word_id,
   })
   return dbCardToCard(dbCard)
@@ -86,33 +111,33 @@ export async function scheduleCard(
   const schedulingCard = f.repeat(currentCard, new Date())[rating] as RecordLogItem
   await createReviewLog(db, { ...schedulingCard.log, head_word_id: currentCard.head_word_id })
 
-  await db.run(card.getUpdateStatement({ ...schedulingCard.card }), {
+  await db.run(cardStatementGenerator.getUpdateStatement({ ...schedulingCard.card }), {
     $id: currentCard.id,
     ...parseParamsToSqlParams(schedulingCard.card),
   })
 
-  const dbCard = await db.get(card.getSelectStatement(), {
+  const dbCard = await db.get(cardStatementGenerator.getSelectStatement(), {
     $id: currentCard.id,
   })
   return dbCardToCard(dbCard)
 }
 
 export async function updateCard(db: DBAPI, { id, ...params }: CardUpdate): Promise<DBCard> {
-  const record = (await db.get(card.getSelectStatement(), {
+  const record = (await db.get(cardStatementGenerator.getSelectStatement(), {
     $id: id,
   })) as DBCard
 
-  await db.run(card.getUpdateStatement(record), {
+  await db.run(cardStatementGenerator.getUpdateStatement(record), {
     $id: id,
     ...parseParamsToSqlParams(record),
     ...parseParamsToSqlParams(params),
   })
 
-  return db.get(card.getSelectStatement(), {
+  return db.get(cardStatementGenerator.getSelectStatement(), {
     $id: id,
   }) as Promise<DBCard>
 }
 
 export async function deleteCard(db: DBAPI, { id }: CardDelete) {
-  return db.run(card.getDeleteStatement(), { $id: id })
+  return db.run(cardStatementGenerator.getDeleteStatement(), { $id: id })
 }
