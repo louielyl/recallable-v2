8270c1c7dcf5329a71feb1df32149557a7ca2214
Louie Lee <louielee.dev@gmail.com>
2024-08-11 09:58:04 +0800

    feat: update definitions by adding findDefinitionsByHeadWordId

--- app/data/crud/definitions.ts
+++ app/data/crud/definitions.ts
@@ -9,35 +9,46 @@ import {
 import { createId } from "@paralleldrive/cuid2"
 import { parseParamsToSqlParams } from "app/utils/sqlParameterParser"
 import { DBAPI, DBStatement } from "./base"
+import { HEAD_WORD_DEFINITION_MAPPINGS_TABLE_NAME } from "../entities/headWordDefinitionMappings"
+import { HEAD_WORDS_TABLE_NAME } from "../entities/headWords"
 
 class DefinitionStatement extends DBStatement {
   constructor(tableName: string) {
     super(tableName)
   }
+  findDefinitionsByHeadWordId(headWordId: string) {
+    return `
+      SELECT d.*, m.[order] from ${this.tableName} d
+      JOIN ${HEAD_WORD_DEFINITION_MAPPINGS_TABLE_NAME} m ON m.definition_id = d.id
+      JOIN ${HEAD_WORDS_TABLE_NAME} h ON h.id = m.head_word_id
+      WHERE h.id = '${headWordId}'
+      ORDER BY m.[order] DESC, m.[created_at] DESC
+    `
+  }
 }
 
-const definition = new DefinitionStatement(DEFINITIONS_TABLE_NAME)
+export const definitionStatementGeneartor = new DefinitionStatement(DEFINITIONS_TABLE_NAME)
 
 export async function createDefinition(
   db: DBAPI,
   { id = createId(), ...params }: DefinitionCreate,
 ): Promise<DBDefinition> {
-  await db.run(definition.getInsertStatement(params), {
+  await db.run(definitionStatementGeneartor.getInsertStatement(params), {
     $id: id,
     ...parseParamsToSqlParams(params),
   })
 
-  return db.get(definition.getSelectStatement(), {
+  return db.get(definitionStatementGeneartor.getSelectStatement(), {
     $id: id,
   }) as Promise<DBDefinition>
 }
 
 export async function findDefinitions(db: DBAPI): Promise<DBDefinition[]> {
-  return db.find(definition.getSelectAllStatement())
+  return db.find(definitionStatementGeneartor.getSelectAllStatement())
 }
 
 export async function getDefinition(db: DBAPI, { id }: DefinitionRead): Promise<DBDefinition> {
-  return db.get(definition.getSelectStatement(), {
+  return db.get(definitionStatementGeneartor.getSelectStatement(), {
     $id: id,
   }) as Promise<DBDefinition>
 }
@@ -46,21 +57,21 @@ export async function updateDefinition(
   db: DBAPI,
   { id, ...params }: DefinitionUpdate,
 ): Promise<DBDefinition> {
-  const record = (await db.get(definition.getSelectStatement(), {
+  const record = (await db.get(definitionStatementGeneartor.getSelectStatement(), {
     $id: id,
   })) as DBDefinition
 
-  await db.run(definition.getUpdateStatement(record), {
+  await db.run(definitionStatementGeneartor.getUpdateStatement(record), {
     $id: id,
     ...parseParamsToSqlParams(record),
     ...parseParamsToSqlParams(params),
   })
 
-  return db.get(definition.getSelectStatement(), {
+  return db.get(definitionStatementGeneartor.getSelectStatement(), {
     $id: id,
   }) as Promise<DBDefinition>
 }
 
 export async function deleteDefinition(db: DBAPI, { id }: DefinitionDelete) {
-  return db.run(definition.getDeleteStatement(), { $id: id })
+  return db.run(definitionStatementGeneartor.getDeleteStatement(), { $id: id })
 }
