ccf92eac5e041c2c78f43765a92e94ecd6966b6c
Louie Lee <louielee.dev@gmail.com>
2024-08-23 09:44:45 +0800

    feat: update definition

--- app/data/entities/definitions.ts
+++ app/data/entities/definitions.ts
@@ -7,15 +7,16 @@ export type DBDefinition = DBTimestampBase & {
   language: string
   source: string | null
   is_verb: number
+  is_transitive_verb: number
+  is_intransitive_verb: number
   is_adjective: number
   is_comparative_adjective: number
   is_superlative_adjective: number
+  is_noun: number
   is_countable_noun: number
   is_uncountable_noun: number
   is_singular_noun: number
   is_plural_noun: number
-  is_transitive_verb: number
-  is_intransitive_verb: number
   is_adverb: number
   is_conjunction: number
   is_determiner: number
@@ -33,15 +34,16 @@ export type Definition = TimestampBase & {
   language: string
   source: string | null
   is_verb: boolean
+  is_transitive_verb: boolean
+  is_intransitive_verb: boolean
   is_adjective: boolean
   is_comparative_adjective: boolean
   is_superlative_adjective: boolean
+  is_noun: boolean
   is_countable_noun: boolean
   is_uncountable_noun: boolean
   is_singular_noun: boolean
   is_plural_noun: boolean
-  is_transitive_verb: boolean
-  is_intransitive_verb: boolean
   is_adverb: boolean
   is_conjunction: boolean
   is_determiner: boolean
@@ -53,6 +55,30 @@ export type Definition = TimestampBase & {
   is_exclamation: boolean
   is_interjection: boolean
 }
+
+export const partOfSpeechToAbbreviation = {
+  is_verb: "v.",
+  is_transitive_verb: "vt.",
+  is_intransitive_verb: "vi.",
+  is_adjective: "adj.",
+  is_comparative_adjective: "comp. adj.",
+  is_superlative_adjective: "superl. adj.",
+  is_noun: "n.",
+  is_countable_noun: "count n.",
+  is_uncountable_noun: "uncount n.",
+  is_singular_noun: "sing. n.",
+  is_plural_noun: "pl. n.",
+  is_adverb: "adv.",
+  is_conjunction: "conj.",
+  is_determiner: "det.",
+  is_preposition: "prep.",
+  is_predeterminer: "predet.",
+  is_pronoun: "pron.",
+  is_prefix: "pref.",
+  is_sufffix: "suff.",
+  is_exclamation: "excl.",
+  is_interjection: "interj.",
+} as const
 
 export type DefinitionCreate = Partial<Definition> & Pick<Definition, "content" | "language">
 export type DefinitionRead = Pick<Definition, "id">
@@ -69,15 +95,16 @@ export const DEFINITIONS_DDL = `
     "language" TEXT NOT NULL,
     "source" TEXT DEFAULT 'recallable',
     "is_verb" BOOLEAN DEFAULT FALSE,
+    "is_transitive_verb" BOOLEAN DEFAULT FALSE,
+    "is_intransitive_verb" BOOLEAN DEFAULT FALSE,
     "is_adjective" BOOLEAN DEFAULT FALSE,
     "is_comparative_adjective" BOOLEAN DEFAULT FALSE,
     "is_superlative_adjective" BOOLEAN DEFAULT FALSE,
+    "is_noun" BOOLEAN DEFAULT FALSE,
     "is_countable_noun" BOOLEAN DEFAULT FALSE,
     "is_uncountable_noun" BOOLEAN DEFAULT FALSE,
     "is_singular_noun" BOOLEAN DEFAULT FALSE,
     "is_plural_noun" BOOLEAN DEFAULT FALSE,
-    "is_transitive_verb" BOOLEAN DEFAULT FALSE,
-    "is_intransitive_verb" BOOLEAN DEFAULT FALSE,
     "is_adverb" BOOLEAN DEFAULT FALSE,
     "is_conjunction" BOOLEAN DEFAULT FALSE,
     "is_determiner" BOOLEAN DEFAULT FALSE,
