ee183547056708e5145535a5751c42e574dbc16f
Louie Lee <louielee.dev@gmail.com>
2024-08-29 00:09:05 +0800

    feat: add worddetail modal to review stack

--- app/screens/ReviewStack/ReviewStack.tsx
+++ app/screens/ReviewStack/ReviewStack.tsx
@@ -2,10 +2,13 @@ import React from "react"
 import { Front } from "./Front"
 import { Back } from "./Back"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Platform } from "react-native"
+import { CollectionWordDetail } from "../CollectionStack/CollectionWordDetail"
 
 export type ReviewParamList = {
   Front: undefined
   Back: { headWord: string }
+  WordDetailEdit: { headWord: string; isEdit: boolean }
 }
 
 const Stack = createNativeStackNavigator<ReviewParamList>()
@@ -21,6 +24,19 @@ export function ReviewStack() {
     >
       <Stack.Screen name="Front" component={Front} options={{ animation: "slide_from_right" }} />
       <Stack.Screen name="Back" component={Back} />
+      {/* TODO: Remove the code duplication in CollectionStack */}
+      <Stack.Group
+        screenOptions={{
+          presentation: Platform.select({
+            ios: "modal",
+            android: "containedTransparentModal",
+          }),
+          animation: "slide_from_bottom",
+          animationDuration: 100,
+        }}
+      >
+        <Stack.Screen name="WordDetailEdit" component={CollectionWordDetail} />
+      </Stack.Group>
     </Stack.Navigator>
   )
 }
